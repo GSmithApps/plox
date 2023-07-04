f1e202ba7d7b2c64e8c89d1c1670742bae49e62a
GSmithApps <14.gsmith.14@gmail.com>
2023-07-04 17:12:53 -0500

    small changes

--- plox/token/get_next_token_ignoring_whitespace.py
+++ plox/token/get_next_token_ignoring_whitespace.py
@@ -1,7 +1,10 @@
 """
-A function that, when given a string of source code, it will return the first token.
+A function that, when given a string of source code,
+it will return the first token and the number of new lines
+in that token (if it is a string literal).
 """
 
+from typing import Tuple
 from .token import Token
 from .tokentype import TokenType
 
@@ -18,6 +21,6 @@ def test_get_next_token_ignoring_whitespace_multiple_tokens():
     assert get_next_token_ignoring_whitespace('()') == Token(TokenType.LEFT_PAREN, '(', None, 1)
 
 
-def get_next_token_ignoring_whitespace(text: str) -> Token:
-    text = text.lstrip()
+def get_next_token_ignoring_whitespace(source_code: str) -> Tuple[Token, int]:
+    source_code = source_code.lstrip()
 
